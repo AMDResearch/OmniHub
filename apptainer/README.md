2119676a16ae23bdec57888fffabe3b09d780de6
Jordà <jordap@users.noreply.github.com>
2024-07-31 11:29:27 -0700

    Keep torchrun and mpi versions of fine-tuning for testing (#13)
    * Match script name to available script in Jose's branch
    
    * Keep multinode torchrun pipeline
    
    * Update apptainer images to include IB
    
    * Update Apptainer documentation
    
    * Keep torchrun and mpi versions of the slurm job
    
    * Add torchrun-based fine-tuning DDP script
    
    * Update image availability
    
    * Tweak and describe torchrun execution
    
    * Log stdout/stderr for each process
    
    * Add script to execute fine tuning without torchrun
    
    * Indent execution levels
    
    * Tweak to match other multinode slurm jobs
    
    * Add missing backslash

--- apptainer/README.md
+++ apptainer/README.md
@@ -1,5 +1,30 @@
-## Build Apptainer image
+## Apptainer
+
+The Apptainer image is based on the Docker image, automatically converted from
+the Dockerfile using `spython`. For now, Omnitrace needs to be disabled in the
+Apptainer builds.
+
+Apptainer images are available in two flavors: `torchrun` and `mpi`. Both
+images are very similar, but meant to be launched in slightly different
+environments.
+
+In the HPC Fund cluster, pre-built apptainer images for both variants are
+available in the shared directory:
+ - `/work1/amd/omnihub/apptainer/omnihub-torchrun.sif`.
+ - `/work1/amd/omnihub/apptainer/omnihub-mpi.sif`.
+
+## Build images
+
+To build your own images:
 
 ```
-apptainer build $WORK/omnihub.sif apptainer/omnihub-ubuntu-dev.def
+apptainer build $WORK/omnihub-torchrun.sif apptainer/omnihub-ubuntu-dev-torchrun.def
+apptainer build $WORK/omnihub-mpi.sif apptainer/omnihub-ubuntu-dev-mpi.def
+```
+
+## Run container
+
+```
+apptainer run --rocm /work1/amd/omnihub/apptainer/omnihub-torchrun.sif
+apptainer run --rocm /work1/amd/omnihub/apptainer/omnihub-mpi.sif
 ```
---
+++ slurm/fine_tune_apptainer_torchrun.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH -J omnihub
+#SBATCH -o %j-slurm.out
+#SBATCH -N 2
+#SBATCH --tasks-per-node=1
+#SBATCH -t 01:00:00
+#SBATCH -p mi2104x
+
+omnihub_dir=$HOME/src/omnihub
+shared_dir=/work1/amd/omnihub
+results_dir=$WORK/results/omnihub/$SLURM_JOB_ID
+model_dir=$shared_dir/ml-models/Meta-Llama-2-7B-Chat-safetensors
+
+head_host=$(ip -f inet addr show eth0  | awk '/inet / {print $2}' | cut -d/ -f1)
+head_port=29400
+num_gpus=4
+
+mkdir -p $results_dir
+
+echo "Omnihub directory: $omnihub_dir"
+echo "Shared directory: $shared_dir"
+echo "Results directory: $results_dir"
+echo "Model directory: $model_dir"
+echo "Head node: $head_host"
+echo "Head port: $head_port"
+echo "Number of GPUs per node: $num_gpus"
+
+export LOGLEVEL=INFO
+export NCCL_DEBUG=INFO
+export NCCL_ENABLE_DMABUF_SUPPORT=0
+export NCCL_IB_DISABLE=0
+export NCCL_P2P_DISABLE=0
+
+# srun to launch one apptainer task per node; inside of the container, launch
+# one process per GPU using torchrun.
+srun \
+    apptainer run --rocm \
+        $shared_dir/apptainer/omnihub-torchrun.sif -c "cd $results_dir; \
+        torchrun \
+            --nnodes=$SLURM_JOB_NUM_NODES \
+            --nproc_per_node=$num_gpus \
+            --master_addr=$head_host \
+            --master_port=$head_port \
+            --node-rank=\$SLURM_PROCID \
+            --log-dir $results_dir \
+            --redirect 3 \
+            $omnihub_dir/scripts/hf-fine-tune-ddp-torchrun.py --ddp -p $model_dir"
